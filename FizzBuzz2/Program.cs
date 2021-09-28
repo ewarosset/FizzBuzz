f8b91dc879f71edc1a9f7c9cb1afc7936795b44c
Ewa <ewa.rosset@softwire.com>
2021-09-28 11:28:56 +0100

    print Fizz for multiples of 3

--- FizzBuzz2/Program.cs
+++ FizzBuzz2/Program.cs
@@ -9,7 +9,15 @@ namespace FizzBuzz2
 
             for (int i = 1; i < 101; i++)
             {
-                Console.WriteLine(i);
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i); 
+                }     
+                
             }
             
         }
