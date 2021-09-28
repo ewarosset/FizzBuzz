485acee36d8fc00704a2478da32d235d9cf775fb
Ewa <ewa.rosset@softwire.com>
2021-09-28 11:32:18 +0100

    print Buzz for multiples of 5 and FizzBuzz for multiples of 3 AND 5

--- FizzBuzz2/Program.cs
+++ FizzBuzz2/Program.cs
@@ -9,9 +9,21 @@ namespace FizzBuzz2
 
             for (int i = 1; i < 101; i++)
             {
-                if (i % 3 == 0)
+
+                if (i % 3 == 0 && i % 5 == 0)
+
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+
+                else if (i % 3 == 0)
                 {
                     Console.WriteLine("Fizz");
+                }
+
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
                 }
                 else
                 {
